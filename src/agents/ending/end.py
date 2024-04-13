0f77b2d8c206bf42d22b695ffa5095e55436caa3
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2024-04-13 12:20:15 +0530

    Revert "s"
    This reverts commit e465a74828c2864e48b48b013c96900474996745.

--- src/agents/ending/end.py
+++ src/agents/ending/end.py
@@ -36,7 +36,7 @@ async def startup(ctx: Context):
 
 
 def send_shared_link_data(data: Response) -> str:
-    url = 'https://ncert-tutor-dev-dbkt.3.us-1.fl0.io/sharedlink'
+    url = 'http://127.0.0.1:8080/sharedlink'
     response = requests.post(url, json=data.dict())
 
     if response.status_code == 200:
